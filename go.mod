192e9e4ce218b162226dd9afad8a00dcfc4ba1e7
biplov <biplovthapa890@gmail.com>
2026-07-18 14:06:14 +0545

    reword

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module kmeans
+module github.com/billuop123/k-means
 
-go 1.24.5
+go 1.26.4
--- main.go
+++ main.go
@@ -3,133 +3,81 @@ package main
 import (
 	"fmt"
 	"math"
-	"math/rand/v2"
+	"math/rand"
 )
 
-type Point struct {
+type coord struct {
+	X       float64
+	Y       float64
+	cluster int
+}
+type centroid struct {
 	X float64
 	Y float64
 }
 
 func main() {
-	data := make([]Point, 5)
-	const k int = 3
-	seededData, centroid := seedData(data, 5, k)
-	cluster := initialCluster(seededData, k, centroid)
-	for {
-		newCluster := (clusterCalculation(cluster))
-		// fmt.Println(newCluster)
-		if equalClusters(newCluster, cluster) {
-			break
-		}
-		cluster = newCluster
+	maxLimit := 1000
+	minLimit := 0
+	_ = minLimit
+	var c []coord
+	coordNum := 50
+	k := 3
+	scaleFactor := float64(coordNum) / float64(maxLimit)
+	for range coordNum {
+		xRand := rand.Float64() * float64(maxLimit)
+		yRand := rand.Float64() * float64(maxLimit)
+		c = append(c, coord{
+			X: xRand * float64(scaleFactor),
+			Y: yRand * float64(scaleFactor),
+		})
 	}
-	fmt.Println(len(cluster))
-	printCluster(cluster)
-}
-func seedData(data []Point, totaldata int, k int) ([]Point, []Point) {
-	centroid := make([]Point, k)
-	for i := 0; i < totaldata; i++ {
-		data[i].X = rand.Float64() * 10
-		data[i].Y = rand.Float64() * 10
-		if i < k {
-			centroid[i].X = float64(data[i].X)
-			centroid[i].Y = float64(data[i].Y)
-		}
+	centroids := []centroid{}
+	for i := range k {
+		centroids = append(centroids, centroid{c[i].X, c[i].Y})
 	}
-	return data, centroid
-}
-func initialCluster(seededData []Point, k int, centroid []Point) [][]Point {
-	n := len(seededData)
-	if n < k {
-		panic("The number of data should be more than the number of clusters")
-	}
-	clusters := make([][]Point, k)
-	for i := 0; i < len(seededData); i++ {
-		minDist := distance(seededData[i], centroid[0])
-		minIndex := 0
-		for j, v := range centroid {
-			d := distance(seededData[i], v)
-			if d < minDist {
-				minIndex = j
-				minDist = d
-			}
-		}
-		clusters[minIndex] = append(clusters[minIndex], seededData[i])
-	}
-	return clusters
-}
-func distance(a Point, b Point) float64 {
-	return math.Sqrt((a.X-b.X)*(a.X-b.X) + (a.Y-b.Y)*(a.Y-b.Y))
-}
-func clusterCalculation(cluster [][]Point) [][]Point {
-	centroid := make([]Point, len(cluster))
-	newCluster := make([][]Point, len(cluster))
-	for i, v := range cluster {
-		centroid[i].X, centroid[i].Y = centroidCalc(v...)
-	}
-	for _, v := range cluster {
-		for _, subV := range v {
-			minDist := distance(subV, centroid[0])
-			minIndex := 0
-			for i, c := range centroid {
-				d := distance(c, subV)
-				if d < minDist {
-					minDist = d
-					minIndex = i
+	loops := 0
+	_ = loops
+	assignment := make([]int, coordNum)
+	changed := true
+	for changed {
+		changed = false
+		loops++
+		for i, val := range c {
+			best := 0
+			minD := dist(centroid{X: val.X, Y: val.Y}, centroids[best])
+			for j := range k {
+				d := dist(centroid{X: val.X, Y: val.Y}, centroids[j])
+				if d < minD {
+					minD = d
+					best = j
 				}
 			}
-			newCluster[minIndex] = append(newCluster[minIndex], subV)
-		}
-	}
-	return newCluster
-}
-func centroidCalc(values ...Point) (float64, float64) {
-	n := len(values)
-	sumX := 0.0
-	sumY := 0.0
-	for i := 0; i < n; i++ {
-		sumX += values[i].X
-		sumY += values[i].Y
-	}
-	return sumX / float64(n), sumY / float64(n)
-}
-func equalClusters(a, b [][]Point) bool {
-	if len(a) != len(b) {
-		return false
-	}
-	for i := range a {
-		if len(a[i]) != len(b[i]) {
-			return false
-		}
-		for j := range a[i] {
-			if a[i][j] != b[i][j] {
-				return false
+			if assignment[i] != best {
+				changed = true
 			}
+			assignment[i] = best
+			c[i].cluster = best
 		}
+		average := make([]centroid, k)
+		count := make([]int, k)
+		for i := range c {
+			average[c[i].cluster].X += c[i].X
+			average[c[i].cluster].Y += c[i].Y
+			count[c[i].cluster] += 1
+		}
+		for j := range average {
+			average[j].X = average[j].X / float64(count[j])
+			average[j].Y = average[j].Y / float64(count[j])
+		}
+		centroids = average
 	}
-	return true
+	for _, val := range c {
+		fmt.Printf("%f, %f      ======>cluster %d\n", val.X, val.Y, val.cluster)
+	}
+	fmt.Printf("Total number of loops %d", loops)
 }
-func printCluster(finalCluster [][]Point) {
-	grid := make([][]rune, 12)
-	for i := range grid {
-		grid[i] = make([]rune, 12)
-		for j := range grid[i] {
-			grid[i][j] = '.'
-		}
-	}
-	sym := [3]rune{'o', '*', '#'}
-	for i, outer := range finalCluster {
-		for _, inner := range outer {
-			x := int(math.Round(inner.X))
-			y := int(math.Round(inner.Y))
-			grid[y][x] = sym[i]
-		}
-	}
-	for _, outer := range grid {
-		for _, inner := range outer {
-			fmt.Printf("%c  ", inner)
-		}
-		fmt.Println("")
-	}
+
+func dist(x, y centroid) float64 {
+	return math.Sqrt((x.Y-y.Y)*(x.Y-y.Y) - (y.X-y.X)*(y.X-y.X))
 }
